e93454bb64d360e38c54782951118d4100eb09dd
lills1 <lilly.stephenson4@gmail.com>
2021-09-21 14:18:31 +1000

    added in the current day to be displayed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,16 @@
 var time=document.getElementById("currentDay");
+var day=document.getElementsByClassName("lead")
 
+//creates a function to display the current time
 function timeDisplay(){
 time = moment().format("hh:mm:ss");
 $("#currentDay").text(time);
-// document.getElementById("currentDay").innerText = time;
 console.log(time);
 }
 
+day=moment().format("dddd, MMMM Do YYYY")
+$(".lead").text(day);
+// calls the function and updates it every second
 setInterval(timeDisplay,1000);
-
 
 
