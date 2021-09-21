c81ead89025b4beae46c1e71a7301b4ac00fd9d6
lills1 <lilly.stephenson4@gmail.com>
2021-09-21 14:10:17 +1000

    created a function to display the time so it updates every second

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,13 @@
 var time=document.getElementById("currentDay");
 
-
+function timeDisplay(){
 time = moment().format("hh:mm:ss");
 $("#currentDay").text(time);
 // document.getElementById("currentDay").innerText = time;
 console.log(time);
-setInterval(time,1000);
+}
+
+setInterval(timeDisplay,1000);
 
 
 
