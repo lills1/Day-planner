bdedcecfc163f2962922c6e2954c2152e5866aab
lills1 <lilly.stephenson4@gmail.com>
2021-09-21 14:03:34 +1000

    added in id tag in jquery to try and fix bugs and get time displayed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,8 +2,8 @@ var time=document.getElementById("currentDay");
 
 
 time = moment().format("hh:mm:ss");
-// $("currentDay").text(time);
-document.getElementById("currentDay").innerText = time;
+$("#currentDay").text(time);
+// document.getElementById("currentDay").innerText = time;
 console.log(time);
 setInterval(time,1000);
 
