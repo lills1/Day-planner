66337dad25e4d15e6ef32775de069792096ecd91
lills1 <lilly.stephenson4@gmail.com>
2021-09-21 14:31:02 +1000

    changed selectors accordingly

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
 var time=document.getElementById("currentDay");
-var day=document.getElementsByClassName("lead")
+var day=document.getElementsByClassName("day")
 
 //creates a function to display the current time
 function timeDisplay(){
@@ -9,8 +9,26 @@ console.log(time);
 }
 
 day=moment().format("dddd, MMMM Do YYYY")
-$(".lead").text(day);
+$(".day").text(day);
 // calls the function and updates it every second
 setInterval(timeDisplay,1000);
+
+function check_time(){
+    // select all time classes
+    let nodes = document.querySelectorAll('.d-flex');
+    for (let node of nodes ) {
+      // should get the time using Date(), but for this example a fixed hour always works ;)
+      const current_time = 12; 
+      // get the time from a data-attribute, don't use an id for this!
+      const node_time = parseInt(node.getAttribute('data-time'));
+      // add a relevant class, much easier to maintain/update than styling elements inside your JS
+      if(current_time === node_time )
+        node.classList.add('now');
+      else if(current_time > node_time )
+        node.classList.add('past');
+      else if(current_time < node_time )
+        node.classList.add('future');
+    }
+  }
 
 
