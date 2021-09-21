84c767a779c6630fa9dfcb30500d3a2fe01287c2
lills1 <lilly.stephenson4@gmail.com>
2021-09-21 13:49:12 +1000

    played around with the vanilla and jquery to get time working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,4 +2,10 @@ var time=document.getElementById("currentDay");
 
 
 time = moment().format("hh:mm:ss");
-$("currentDay").text(time);
+// $("currentDay").text(time);
+document.getElementById("currentDay").innerText = time;
+console.log(time);
+setInterval(time,1000);
+
+
+
